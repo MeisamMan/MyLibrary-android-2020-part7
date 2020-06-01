06f4f2ce706b3d6a72fc1290000b064e0a552e6a
Meisam <Meisam@meiCode.org>
2020-06-02 01:45:41 +0400

    The source code for the video called 'Create a Delete Button' in the Android App Development Master Class for 2020

---
+++ app/src/main/java/org/meicode/mylibrary/MainActivity.java
@@ -0,0 +1,72 @@
+package org.meicode.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnAlreadyRead, btnWantToRead, btnCurrentlyReading, btnFavorite, btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        btnAllBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAlreadyRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AlreadyReadBookActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCurrentlyReading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CurrentlyReadingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnWantToRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WantToReadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FavoriteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Utils.getInstance();
+    }
+
+    private void initViews() {
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnWantToRead = findViewById(R.id.btnWantToRead);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnFavorite = findViewById(R.id.btnFavorite);
+        btnAbout = findViewById(R.id.btnAbout);
+    }
+}
